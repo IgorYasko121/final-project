6efa95bf501db87e67bb3ef9cefb9c71e6f375de
Igor Yasko <41817685+IgorYasko121@users.noreply.github.com>
2019-08-20 16:30:06 +0300

    Update AbstractDAO.java

--- src/main/java/com/igoryasko/justmusic/dao/AbstractDAO.java
+++ src/main/java/com/igoryasko/justmusic/dao/AbstractDAO.java
@@ -3,7 +3,7 @@ package com.igoryasko.justmusic.dao;
 import java.sql.Connection;
 
 /**
- * The {@code AbstractDAO} class provides methods for working with the database.
+ * The {@code AbstractDAO} class provides connection for working with the database.
  * @author Igor Yasko on 2019-07-19.
  */
 public abstract class AbstractDAO {
